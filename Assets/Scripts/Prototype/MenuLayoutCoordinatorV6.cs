efda678e81881a3ace273cee064a9b889e270c23
sungsoo <142865231+sslee1210@users.noreply.github.com>
2026-07-23 11:20:59 +0900

    Fix missing StageFlowController namespace import

--- Assets/Scripts/Prototype/MenuLayoutCoordinatorV6.cs
+++ Assets/Scripts/Prototype/MenuLayoutCoordinatorV6.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
+using PeanutWarrior.Core;
 using UnityEngine;
 
 namespace PeanutWarrior.Prototype
